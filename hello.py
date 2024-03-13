745b6d93a46db731f41b4167f5f939a304026c5b
pratik_codes <66104012+pratik123-coder@users.noreply.github.com>
2024-03-13 15:47:45 +0530

    Sample Data Added || Code Added

---
+++ hello.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+
+
+def merge(folder_path):
+    combined_data = pd.DataFrame()
+
+    for file_name in os.listdir(folder_path):
+        if 'closePosition' in file_name and file_name.endswith('.csv'):
+            file_path = os.path.join(folder_path, file_name)
+            df = pd.read_csv(file_path, usecols=['Key', 'ExitTime', 'Symbol', 'EntryPrice', 'Quantity', 'Pnl'])
+            df['Date'] = pd.to_datetime(df['ExitTime']).dt.date
+            combined_data = pd.concat([combined_data, df], ignore_index=True)
+
+
+    combined_data.to_csv('combined_closePosition.csv', index=False)
+
+def calculate(combined_data):
+    total_trades = len(combined_data)
+    unique_days = len(combined_data['Date'].unique())
+    average_trades = total_trades / unique_days
+    total_pnl = combined_data['Pnl'].sum()
+    profit_trades = len(combined_data[combined_data['Pnl'] > 0])
+    loss_trades = len(combined_data[combined_data['Pnl'] <= 0])
+
+    with open('combined_stats.txt', 'w') as file:
+        file.write(f'Total trades: {total_trades}\n')
+        file.write(f'Unique days: {unique_days}\n')
+        file.write(f'Average trades: {average_trades}\n')
+        file.write(f'Total Pnl: {total_pnl}\n')
+        file.write(f'Profit Trades: {profit_trades}\n')
+        file.write(f'Loss Trades: {loss_trades}\n')
+
+
+folder_path = '/Users/pratikmohanty/Desktop/python/SampleData/SampleData'
+# Give your Folder Path
+merge(folder_path)
+
+combined_data = pd.read_csv('combined_closePosition.csv')
+calculate(combined_data)
+
